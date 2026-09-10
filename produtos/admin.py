1c4437a39cbffe6f35c8d829a2c2def97c7426e6
matheusvmb2308 <matheusvmbernardes@gmail.com>
2026-09-10 08:59:20 -0300

    feat(admin): configura gerenciamento de produtos

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "preco")
+    search_fields = ("nome",)
